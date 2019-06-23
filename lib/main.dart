4875ff0b53c930282476f0ef22a2a53d7591e904
chrisoftech <chrisoftech@gmail.com>
2019-06-23 12:17:50 +0000

    Imported quick-sand fonts & temp images, Added a theme to main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'FashionNet',
       theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
+        fontFamily: 'QuickSand',
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.orange,
       ),
       home: Container(),
     );
