28fc0e0c521c8dd4bfb4f4efcff3e2c78cce51e8
chrisoftech <chrisoftech@gmail.com>
2019-06-26 12:34:07 +0000

    Installed multi_image_picker: ^4.3.4, Inserted camera permisions in android manifest file, Refactored profile-image form & implement  multi-image picker in profile-image form.

--- lib/widgets/profile/profile_imageform.dart
+++ lib/widgets/profile/profile_imageform.dart
@@ -1,35 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
-class ProfileImageForm extends StatelessWidget {
-  Widget _buildImageContainer() {
+class ProfileImageForm extends StatefulWidget {
+  @override
+  _ProfileImageFormState createState() => _ProfileImageFormState();
+}
+
+class _ProfileImageFormState extends State<ProfileImageForm> {
+  List<Asset> images = List<Asset>();
+  String _error = 'No Error Dectected';
+
+  Future<void> deleteAssets() async {
+    await MultiImagePicker.deleteImages(assets: images);
+    setState(() {
+      images = List<Asset>();
+    });
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+    String error = 'No Error Dectected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+          maxImages: 1,
+          enableCamera: true,
+          selectedAssets: images,
+          cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+          materialOptions: MaterialOptions(
+            actionBarColor: "#abcdef",
+            actionBarTitle: "FashioNet",
+            allViewTitle: "All Photos",
+            selectCircleStrokeColor: "#000000",
+          ));
+    } on PlatformException catch (e) {
+      error = e.message;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      _error = error;
+    });
+  }
+
+  Widget _buildDisplayedImage() {
+    if (images.isEmpty) {
+      return Container(
+        height: 200.0,
+        width: 200.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100.0),
+          child: Image.asset('assets/avatars/ps-avatar.png', fit: BoxFit.cover),
+        ),
+      );
+    } else {
+      Asset asset = images[0];
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(100.0),
+        child: AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 300,
+        ),
+      );
+    }
+  }
+
+  Widget _buildStackOverlayIcon() {
     return Container(
-      height: 200.0,
-      width: 200.0,
       decoration: BoxDecoration(
-        color: Colors.grey,
+        color: Colors.black38,
         shape: BoxShape.circle,
-        border: Border.all(width: 2.0, color: Colors.white70),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('assets/images/temp7.jpg'),
+      ),
+      child: Center(
+        child: Icon(
+          images.isEmpty ? Icons.camera_alt : Icons.refresh,
+          color: Colors.white24,
+          size: 70.0,
         ),
       ),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.black54,
-              shape: BoxShape.circle,
-            ),
-            child: Center(
-              child: Icon(
-                Icons.refresh,
-                color: Colors.white54,
-                size: 70.0,
-              ),
-            ),
-          ),
-        ],
+    );
+  }
+
+  Widget _buildImageContainer() {
+    return InkWell(
+      splashColor: Colors.yellow,
+      borderRadius: BorderRadius.circular(100.0),
+      onTap: loadAssets,
+      child: Container(
+        height: 200.0,
+        width: 200.0,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          shape: BoxShape.circle,
+          border: Border.all(width: 2.0, color: Colors.white70),
+        ),
+        child: Stack(
+          children: <Widget>[
+            _buildDisplayedImage(),
+            _buildStackOverlayIcon(),
+          ],
+        ),
       ),
     );
   }
