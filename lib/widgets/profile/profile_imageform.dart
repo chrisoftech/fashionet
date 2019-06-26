de439daa635e666a47a905e591a7dcc53a4d596d
chrisoftech <chrisoftech@gmail.com>
2019-06-26 00:16:30 +0000

    Refactored profile-image form.dart

--- lib/widgets/profile/profile_imageform.dart
+++ lib/widgets/profile/profile_imageform.dart
@@ -1,6 +1,56 @@
 import 'package:flutter/material.dart';
 
 class ProfileImageForm extends StatelessWidget {
+  Widget _buildImageContainer() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle,
+        border: Border.all(width: 2.0, color: Colors.white70),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/temp7.jpg'),
+        ),
+      ),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black54,
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Icon(
+                Icons.refresh,
+                color: Colors.white54,
+                size: 70.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImageMessage({@required BuildContext context}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          Icons.info,
+          color: Theme.of(context).accentColor,
+        ),
+        SizedBox(width: 5.0),
+        Text(
+          'Select a profile avatar',
+          style: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -8,54 +58,11 @@ class ProfileImageForm extends StatelessWidget {
         child: Container(
           height: 250.0,
           width: 400.0,
-          // color: Colors.teal,
           child: Column(
             children: <Widget>[
-              Container(
-                height: 200.0,
-                width: 200.0,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  shape: BoxShape.circle,
-                  border: Border.all(width: 2.0, color: Colors.white70),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/images/temp7.jpg'),
-                  ),
-                ),
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.black54,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.refresh,
-                          color: Colors.white54,
-                          size: 70.0,
-                        ),
-                      ),
-                    ), 
-                  ],
-                ),
-              ),
+              _buildImageContainer(),
               SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.info,
-                    color: Theme.of(context).accentColor,
-                  ),
-                  SizedBox(width: 5.0),
-                  Text(
-                    'Select a profile avatar',
-                    style: TextStyle(color: Colors.white, fontSize: 20.0),
-                  ),
-                ],
-              ),
+              _buildImageMessage(context: context),
             ],
           ),
         ),
